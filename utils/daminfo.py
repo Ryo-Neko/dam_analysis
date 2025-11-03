bcfd94255196605ba7b783ee4deb8a6abcffcad4
Ryo Kaneko <deepkneko@gmail.com>
2025-11-03 23:15:36 +0900

    asap

--- utils/daminfo.py
+++ utils/daminfo.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 
 class DamInfo:
-    def __init__(self, dam_id_path="fixed_out/site_ids_dam.csv"):
+    def __init__(self, dam_id_path="../SuimonSuishituDB/fixed_out/site_ids_dam.csv"):
         self.dam_id = pd.read_csv(dam_id_path, header=0)
 
-    def convert_id2name(self, dam_id):
-        return self.dam_id.loc[self.dam_id["id"] == dam_id, "観測所名"].to_list()
+    def convert_id2name(self, target_id):
+        return self.dam_id.loc[self.dam_id["id"] == int(target_id), "観測所名"].to_list()
 
-    def convert_id2riverseries_name(self, dam_id):
-        return self.dam_id.loc[self.dam_id["id"] == dam_id, "水系名"].to_list()
+    def convert_id2riverseries_name(self, target_id):
+        return self.dam_id.loc[self.dam_id["id"] == int(target_id), "水系名"].to_list()
 
-    def convert_id2river_name(self, dam_id):
-        return self.dam_id.loc[self.dam_id["id"] == dam_id, "河川名"].to_list()
+    def convert_id2river_name(self, target_id):
+        return self.dam_id.loc[self.dam_id["id"] == int(target_id), "河川名"].to_list()
 
     def convert_rivername2id(self, rivername):
         return self.dam_id.loc[self.dam_id["河川名"] == rivername, "id"].to_list()
